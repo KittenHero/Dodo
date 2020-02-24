f9ef19e51b4b092c51196e566f7dacd2c634fbc3
HamRanpara <gyradoshr@gmail.com>
2020-02-24 23:54:12 +0300

    Fixing Errors
    Letting Godot safely disable.
    Unused arguments.

--- src/Actors/Enemies/Enemy.gd
+++ src/Actors/Enemies/Enemy.gd
@@ -9,7 +9,8 @@ func _ready() -> void:
 func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
 	if body.global_position.y > get_node("StompDetector").global_position.y:
 		return
-	get_node("CollisionShape2D").disabled = true
+	# Godot waits till its safe to disable
+	$CollisionShape2D.set_deferred("disabled", true)
 	queue_free()
 
 
