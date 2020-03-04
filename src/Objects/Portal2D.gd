dd899ca236654686ed851f188b4b78b494f9411b
NarisR <nariscatboy@gmail.com>
2020-03-04 21:07:04 +1100

    Pause game during portal animation

--- src/Objects/Portal2D.gd
+++ src/Objects/Portal2D.gd
@@ -17,9 +17,14 @@ func _get_configuration_warning() -> String:
 
 
 func teleport() -> void:
+	var initial_pause_mode = pause_mode
+	pause_mode = Node.PAUSE_MODE_PROCESS
+	get_tree().paused = true
 	anim_player.play("FADE_IN")
 	# yield waits until node/func mentioned to emit a kind of signal
 	yield(anim_player, "animation_finished")
+	get_tree().paused = false
+	pause_mode = initial_pause_mode
 	get_tree().change_scene_to(next_scene)
 
 
