c2e7ab4471f218eb7a4f2d94c42498489ee194d3
NarisR <nariscatboy@gmail.com>
2020-04-18 12:39:21 +1000

    fix dive and jump

--- src/Actors/Player/PlayerJumping.gd
+++ src/Actors/Player/PlayerJumping.gd
@@ -3,19 +3,15 @@ extends State
 func physics_process(parent: KinematicBody2D, delta: float):
 	var velocity = parent.move_horizontal(delta)
 	velocity.y += parent.gravity * delta
-	var collision = parent.move_and_collide(velocity * delta)
+	parent.velocity = parent.move_and_slide(velocity, Vector2.UP)
 	
-	if velocity.y >= 0 or collision:
+	if parent.velocity.y >= 0:
 		parent.set_state(parent.STATES.FALLING)
 	elif Input.is_action_just_pressed("jump"):
 		parent.set_state(parent.STATES.DIVE)
-	elif parent.is_on_floor():
-		parent.set_state(parent.STATES.RUNNING)
-
 
 func enter(parent: KinematicBody2D):
 	parent.velocity.y = - sqrt(2 * parent.jump_height * parent.gravity)
-
 
 func exit(_parent: KinematicBody2D):
 	pass
